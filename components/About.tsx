26af8a1ba1aabbb878ebca2b9581764dd0f94728
RONIT B PATEL <ronitpatel1810@gmail.com>
2023-10-31 21:52:42 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -11,7 +11,7 @@ const About = () => {
         <div className=' text-sm md:text-xl w-fit h-fit bg-gray-200 bg-opacity-[0.3] rounded-xl p-5 md:p-10'>
           <span className=' text-2xl md:text-4xl font-bold font-main '>Hello,</span><br/> I am Ronit Patel, from India pursuing my Bachelor of Technology in the field of Computer Science & Engineering from JD. College of Engineering and Management (Nagpur) which is affiliated with Dr.Babasaheb Ambedkar Technological University, with a passion for developing scalable web-applications and focused on full stack development.
 <br/> <br/>
-I started my development journey as a hobby in technology, and to understand the foundation of computers, I chose Computer Science and Engineering and this is where I got introduced to the amazing field of Web-Developments. The shift in my career happened when I started self-learning the new developing techniques and tools which made me learn more about technology.
+I started my development journey as a hobby in technology, and to understand the foundation of computers, I chose Computer Science and Engineering and this is where I got introduced to the amazing field of Web-Developments. The shift in my career happened when I started self-learning the new developing techniques and tools which made me learn more about technology.<br/><br/>
 I am an enthusiastic and motivated team player with great imagination and excellent organizational skills. I enjoy taking on challenging tasks that encourage me to think and create outside the box and contribute to my personal growth.
           
         </div>
