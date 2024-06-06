70b7f66836d0dbd926778003dc69b1b86dfc57ce
RONIT B PATEL <ronitpatel1810@gmail.com>
2024-06-06 14:46:47 +0530

    Update SingleProject.tsx

--- components/SingleProject.tsx
+++ components/SingleProject.tsx
@@ -14,8 +14,10 @@ const SingleProject = (props:any) => {
         <div className=' p-10 w-full h-96 '>
             <h1 className=' mb-3 md:mb-5 text-xl md:text-4xl font-semibold md:font-bold'>{unitProject.name}</h1>
             <p className=' mb-3 md:mb-5 text-gray-500'>{unitProject.about}</p>
-            <Link className=' flex justify-center items-center w-fit px-4 py-3 gap-2 hover:text-sky-900 bg-gray-200 rounded-lg text-lg font-medium' href={unitProject.link}>Visit <AiOutlineGithub /> </Link>
-            {deplyedData?<Link className=' mb-4 flex justify-center items-center w-fit px-4 py-3 gap-2 hover:text-sky-900 bg-gray-200 rounded-lg text-lg font-medium' href={deplyedData}>Deployed Link <FiExternalLink /></Link>:null}
+            <div className=' flex gap-4'>
+              <Link className=' mb-4 flex justify-center items-center w-fit px-4 py-3 gap-2 hover:text-sky-900 bg-gray-200 rounded-lg text-lg font-medium' href={unitProject.link}>Visit <AiOutlineGithub /> </Link>
+              {deplyedData?<Link className=' mb-4 flex justify-center items-center w-fit px-4 py-3 gap-2 hover:text-sky-900 bg-gray-200 rounded-lg text-base md:text-lg font-medium' href={deplyedData}>Deployed Link <FiExternalLink /></Link>:null}
+            </div>
             {/* <button className=' flex justify-center items-center gap-2 text-lg font-medium hover:text-sky-900 py-2 px-4 bg-gray-200 rounded-lg border-[1px]'>Visit <AiOutlineGithub /> </button> */}
         </div>
         <div className=' p-10 relative  w-full h-60 md:h-96 '>
